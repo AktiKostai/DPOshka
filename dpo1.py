478fff6464126f735aabc6ecdc82cccdbc9f2205
AktiKostai <ks-zd@mail.ru>
2024-05-25 21:15:24 +0500

    finish

--- dpo1.py
+++ dpo1.py
@@ -1,31 +1,32 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 class Employee(db.Model):
-    d = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    position = db.Column(db.String(50))
+    count = db.Column(db.String(50))
 
-    def __init__(self, name, position):
+    def __init__(self, name, count):
         self.name = name
-        self.postion = position
+        self.count = count
 
 with app.app_context():
     db.create_all()
+
 @app.route('/add_employee', methods=['POST'])
 def add_employee():
     name = request.form['name']
-    position = request.form['position']
-    employee = Employee(name, position)
+    count = request.form['count']
+    employee = Employee(name, count)
     db.session.add(employee)
     db.session.commit()
     return{"session": "Employee added successfully"}
+
 @app.route('/get_employee/<int:id>')
 def get_employee(id):
     employee = Employee.query.get(id)
@@ -33,11 +34,10 @@ def get_employee(id):
         return jsonify({
             'id': employee.id,
             'name': employee.name,
-            'position': employee.position
+            'count': employee.count
         })
     else:
         return {'error': 'Employee not found'}
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host = "0.0.0.0", debug=True)
